82cd3250afd228c5dcb6dde4cf8da390e68fdf93
Kurban Magomedov <cuba.pro@ya.ru>
2021-11-12 01:02:18 +0300

    Configure browser sync for blade files

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,6 +4,16 @@ const tailwindcss = require('tailwindcss');
 mix.setPublicPath('public/build')
     .setResourceRoot('/build/')
     .js('resources/assets/js/app.js', 'js')
+    .browserSync({
+        proxy: 'localhost',
+        open: false,
+        notify: {
+            styles: {
+                top: 'auto',
+                left: 'auto'
+            }
+        },
+    })
     // .js('resources/assets/js/admin.js', 'js')
     .sass('resources/assets/scss/app.scss', 'css')
     // .sass('resources/assets/scss/admin.scss', 'css')
